ca2f1e2f82fb134abc4a4504be6c8797d06d3969
Lucas Silveira Portal <46923597811@info.local>
2018-12-12 09:05:45 -0200

    up

--- src/components/CadProf.vue
+++ src/components/CadProf.vue
@@ -1,166 +1,179 @@
 <template>
-    <div class="formCadProf">
-        <p>
-        <router-link v-bind:to="{ name: 'menu' }" class="card-text" id="x">X</router-link>
-        </p>
-        <img center id="md" src="@/assets/imagens/LOGO-oficial.png">
-        <h2>Profissionais</h2>
-        <h3>Cadastro</h3>
-        <b-form @submit="onSubmit" >
-            <b-form-group>
-                <b-form-input v-model="nomeCompleto" type="text" placeholder="Nome Completo:" id="form´s"></b-form-input>
-                <b-form-input v-model="email" type="text" placeholder="E-mail:" id="form´s"></b-form-input>
+  <div class="formCadProf">
+    <p>
+      <router-link v-bind:to="{ name: 'menu' }" class="card-text" id="x">X</router-link>
+    </p>
+    <img center id="md" src="@/assets/imagens/LOGO-oficial.png">
+    <h2>Profissionais</h2>
+    <h3>Cadastro</h3>
+    <b-form @submit="onSubmit">
+      <b-form-group>
+        <b-form-input v-model="nomeCompleto" type="text" placeholder="Nome Completo:" id="form´s"></b-form-input>
+        <b-form-input v-model="email" type="text" placeholder="E-mail:" id="form´s"></b-form-input>
 
-                <!--  -->
-                 {{"selecione uma empresa a qual deseja vincular o cadastro"}}
-                <b-form-select v-model="selecionePJ" class="mb-3" id="form´s" @change="buscarCT()">
-                <option v-for="pj in pessoaJuridica" :value="pj.id">{{pj.nomeFantasia}}</option>
-                </b-form-select>
-               
-                <b-form-select v-model="selecioneCT" class="mb-3" id="form´s" @change="buscarCT()">
-                <option v-for="ct in categoria" :value="ct.id">{{ct.nome}}</option>
-                </b-form-select>
-                {{"selecione uma categoria da empresa selecionada a qual deseja vincular o cadastro"}}
+        <!--  -->
+        {{"selecione uma empresa a qual deseja vincular o cadastro"}}
+        <b-form-select class="mb-3" id="form´s" @change.native="myChange" v-model="selecionePJ">
+          <option v-for="pj in pessoaJuridica" :key="pj.id" :value="pj.id">{{pj.nomeFantasia}}</option>
+        </b-form-select>
+        {{selecionePJ}}
+        <b-form-select class="mb-3" id="form´s" @change.native="myChangeCT" v-model="selecioneCT">
+          <option v-for="ct in categoria" :key="ct.id" :value="ct.id">{{ct.nome}}</option>
+        </b-form-select>
+        {{"selecione uma categoria da empresa selecionada a qual deseja vincular o cadastro"}}
+        <b-form-select v-model="selecioneSV" class="mb-3" id="form´s" @change="buscarSV()">
+          <option v-for="sv in servico" :value="sv.id">{{sv.nome}}</option>
+        </b-form-select>
 
-                <b-form-select v-model="selecioneSV" class="mb-3" id="form´s" @change="buscarSV()">
-                <option v-for="sv in servico" :value="sv.id">{{sv.nome}}</option>
-                </b-form-select>
-
-                <b-button type="submit" variant="primary" id>Cadastrar</b-button>
-            </b-form-group>
-        </b-form>
-    </div>
+        <b-button type="submit" variant="primary" id>Cadastrar</b-button>
+      </b-form-group>
+    </b-form>
+  </div>
 </template>
 
 <script>
-import axios from "axios"
+import axios from "axios";
 /*import "./assets/stylesheets/main.css";*/
 export default {
-  name: 'Profissionais',
-  data () {
+  name: "Profissionais",
+  data() {
     return {
-        selecionePJ:"",
-        selecioneCT:"",
-        selecioneSV:"",
-        nomeCompleto: "",
-        email: "",
-      pessoaJuridica:[],
-      categoria:[],
-      servico:[]
+      selecionePJ: "",
+      selecioneCT: "",
+      selecioneSV: "",
+      nomeCompleto: "",
+      email: "",
+      pessoaJuridica: [],
+      categoria: [],
+      servico: []
     };
   },
-  methods:{
-      onSubmit(evt){
-          evt.preventDefault();
-
-          return axios({
-              method: "post",
-              url:"http://localhost:51917/api/profissional/" + localStorage.getItem("idAdm") + "/" + this.selecioneSV,
-              data: {
-                  nomeCompleto: this.nomeCompleto,
-                  email: this.email
-              }
-            }).then;
+  methods: {
+    onSubmit(evt) {
+      evt.preventDefault();
+      return axios({
+        method: "post",
+        url:
+          "http://localhost:51917/api/profissional/" +
+          localStorage.getItem("idAdm") +
+          "/" +
+          this.selecioneSV,
+        data: {
+          nomeCompleto: this.nomeCompleto,
+          email: this.email
         },
-        buscarPJ(){
-            return axios({
-                method: "get",
-                url:"http://localhost:51917/api/pessoaJuridica/" + localStorage.getItem("idAdm"),
-                headers: {
-                    Authorization: "Bearer " + localStorage.getItem("token")
-                }
-            }).then(response =>{
-                this.pessoaJuridica = response.data;
-                console.log(this.pessoaJuridica);
-            }).catch(error => console.log(error));
-        },
-        buscarCT(){
-            let idPj = this.selecionePJ;
-             return axios({
-                 method: "get",
-            // idPJ será o valor do primeiro select
-                 url:"http://localhost:51917/api/categoria/" + localStorage.getItem("idAdm") +  "/" + idPj,
-                 headers: {
-                    Authorization: "Bearer " + localStorage.getItem("token")
-                }
-             }).then(response =>{
-                 this.categoria = response.data;
-                 console.log("--------------------------------------------------------------" +this.categoria);
-             }).catch(error => console.log(error));
-        },
-        buscarSV(){
-            let idct = this.selecioneCT;
-            return axios({
-                 method:"get",
-                  url:"http://localhost:51917/api/servico/" + localStorage.getItem("idAdm") + "/" + idct,
-                  headers: {
-                    Authorization: "Bearer " + localStorage.getItem("token")
-                }
-            }).then(response =>{
-                 this.servico = response.data;
-                console.log(this.servico);
-            }).catch(error => console.log(error));
+        headers: {
+          Authorization: "Bearer " + localStorage.getItem("token")
         }
+      })
+        .then(response => {
+          console.log(response.data);
+        })
+        .catch(error => console.log(error));
     },
-    mounted(){
-        this.buscarPJ();
-        this.buscarCT();
-        this.buscarSV();
-        
+    myChange(evt) {
+      let val = evt.target.value;
+      console.log(val);
+      return axios({
+        method: "get",
+        url:
+          "http://localhost:51917/api/categoria/" +
+          localStorage.getItem("idAdm") +
+          "/" +
+          val,
+        headers: {
+          Authorization: "Bearer " + localStorage.getItem("token")
+        }
+      })
+        .then(response => {
+          this.categoria = response.data;
+        })
+        .catch(error => console.log(error));
+    },
+    myChangeCT(evt) {
+      let val = evt.target.value;
+      return axios({
+        method: "get",
+        url:
+          "http://localhost:51917/api/servico/" +
+          localStorage.getItem("idAdm") +
+          "/" +
+          val,
+        headers: {
+          Authorization: "Bearer " + localStorage.getItem("token")
+        }
+      })
+        .then(response => {
+          this.servico = response.data;
+        })
+        .catch(error => console.log(error));
+    },
+    buscarPJ() {
+      return axios({
+        method: "get",
+        url:
+          "http://localhost:51917/api/pessoaJuridica/" +
+          localStorage.getItem("idAdm"),
+        headers: {
+          Authorization: "Bearer " + localStorage.getItem("token")
+        }
+      })
+        .then(response => {
+          this.pessoaJuridica = response.data;
+          console.log(this.pessoaJuridica);
+        })
+        .catch(error => console.log(error));
     }
+  },
+  mounted() {
+    this.buscarPJ();
+  }
 };
 </script>
 
 <style>
-#md{
-    width: 25em;
-    height: 5em;
-    left: 35%;
+#md {
+  width: 25em;
+  height: 5em;
+  left: 35%;
 }
 
-
-
-
-
-#btn{
-    width: 7em;
-    left: 42em;
-    position: relative;
-    font-size: 1.2em;
+#btn {
+  width: 7em;
+  left: 42em;
+  position: relative;
+  font-size: 1.2em;
 }
 
+#x {
+  width: 5em;
+  margin-top: -20em;
+  margin-left: 20em;
 
-#x{
-    width: 5em;
-    margin-top: -20em;
-    margin-left: 20em;
-    
-    font-weight: 500;
-    text-decoration: none;
-}	
-
-#exit{
-    left: 96%;
-    width: 4%;
-    top: 0%;
-    position: absolute;
+  font-weight: 500;
+  text-decoration: none;
 }
 
-.card-title{
-    font-size: 200%;
+#exit {
+  left: 96%;
+  width: 4%;
+  top: 0%;
+  position: absolute;
 }
 
-#bol3{
-    width: 2.6%;
-    position: relative;
-    left: -15%;
+.card-title {
+  font-size: 200%;
 }
 
-p{
-        left: 5%;
-    position: relative;
-    font-size: 1.5em;
+#bol3 {
+  width: 2.6%;
+  position: relative;
+  left: -15%;
 }
 
-
+p {
+  left: 5%;
+  position: relative;
+  font-size: 1.5em;
+}
 </style>
